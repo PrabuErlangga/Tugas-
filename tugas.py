013f3d51248b8cbf84ce242c0c678f25aa9a7151
PrabuErlangga <154669410+PrabuErlangga@users.noreply.github.com>
2024-01-13 04:03:45 +0700

    Create tugas.py

---
+++ tugas.py
@@ -0,0 +1,93 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.height = 1
+        self.left = None
+        self.right = None
+
+
+class AVLTree:
+    def getHeight(self, node):
+        if node is None:
+            return 0
+        return node.height
+
+    def getBalance(self, node):
+        if node is None:
+            return 0
+        return self.getHeight(node.left) - self.getHeight(node.right)
+
+    def rotateRight(self, y):
+        x = y.left
+        T2 = x.right
+
+        x.right = y
+        y.left = T2
+
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+        x.height = 1 + max(self.getHeight(x.left), self.getHeight(x.right))
+
+        return x
+
+    def rotateLeft(self, x):
+        y = x.right
+        T2 = y.left
+
+        y.left = x
+        x.right = T2
+
+        x.height = 1 + max(self.getHeight(x.left), self.getHeight(x.right))
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+
+        return y
+
+    def insert(self, root, key):
+        if root is None:
+            return Node(key)
+
+        if key < root.key:
+            root.left = self.insert(root.left, key)
+        elif key > root.key:
+            root.right = self.insert(root.right, key)
+        else:
+            return root  # No duplicate keys allowed
+
+        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
+        balance = self.getBalance(root)
+
+        # Left Heavy
+        if balance > 1:
+            if key < root.left.key:
+                return self.rotateRight(root)
+            else:
+                root.left = self.rotateLeft(root.left)
+                return self.rotateRight(root)
+
+        # Right Heavy
+        if balance < -1:
+            if key > root.right.key:
+                return self.rotateLeft(root)
+            else:
+                root.right = self.rotateRight(root.right)
+                return self.rotateLeft(root)
+
+        return root
+
+    def preOrder(self, root):
+        if root is not None:
+            print("{0} ".format(root.key), end="")
+            self.preOrder(root.left)
+            self.preOrder(root.right)
+
+
+# Contoh penggunaan
+avl_tree = AVLTree()
+root = None
+
+keys = [9, 5, 10, 0, 6, 11, -1, 1, 2]
+for key in keys:
+    root = avl_tree.insert(root, key)
+
+print("Preorder traversal hasil AVL Tree:")
+avl_tree.preOrder(root)
